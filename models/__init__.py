00fd65da9ef7a6e4afacc57aabc7c89694f2bf4d
paul <paul.tiwald@gmail.com>
2026-04-19 10:50:01 +0200

    add pt pointnet

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from .fourier_pointnet import FourierPointNet
 from .knn_pointnet import KNNPointNet
 from .mlp import MLP
+from .pt_pointnet import PTPointNet
 from .vanilla_pointnet import VanillaPointNet
